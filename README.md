a71628f6a28d0dddfcad232a7f5c9f09dcd1148a
ElliotNB <elliot.bradshaw@transformativemed.com>
2017-08-10 16:23:02 -0700

    Added pause and resume functionality. Fixed bug where observables array would grow from recursive execution.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Observable Slim
 https://github.com/elliotnb/observable-slim
 
-Version 0.0.1. Copyright 2017, Elliot B. All rights reserved.
+Version 0.0.1.
 
 Licensed under the MIT license:
 
@@ -11,24 +11,25 @@ http://www.opensource.org/licenses/MIT
 Observable Slim is a singleton that allows you to observe changes made to an object and any nested
 children of that object. It is intended to assist with one-way data binding, that is, in MVC parlance, 
 reflecting changes in the model to the view. Observable Slim aspires to be as lightweight and easily
-understood as possible. Minifies down to roughly 500 characters.
+understood as possible. Minifies down to roughly 1500 characters.
 
 ## Usage
 
 ```javascript
 var test = {};
-var p = ObservableSlim.create(test, false, function(changes) {
+var p = ObservableSlim.create(test, true, function(changes) {
 	console.log(JSON.stringify(changes));
 });
 
-p.hello = "world";  		// [{"target":{},"property":"hello","newValue":"world","currentPath":"hello"}]
-p.testing = {}; 			// [{"target":{"hello":"world"},"property":"testing","newValue":{},"currentPath":"testing"}]
-p.testing.blah = 42;		// [{"target":{},"property":"blah","newValue":42,"currentPath":"testing.blah"}]
-p.arr = [];					// [{"target":{"hello":"world","testing":{"blah":42}},"property":"arr","newValue":[],"currentPath":"arr"}]
-p.arr.push("splice test");	// [{"target":[],"property":"0","newValue":"splice test","currentPath":"arr"}]
-p.arr.push("hello world");	// [{"target":["splice test"],"property":"1","newValue":"hello world","currentPath":"arr"}]
-p.arr.splice(0,1);    // [{"target":["splice test","hello world"],"property":"0","newValue":"hello world","previousValue":"splice test","currentPath":"arr"}]
-console.log(JSON.stringify(test));			// {"hello":"world","testing":{"blah":42},"arr":["hello world"]}
+p.hello = "world";  		// [{"type":"add","target":{"hello":"world"},"property":"hello","newValue":"world","currentPath":"hello"}]
+p.testing = {}; 			// [{"type":"add","target":{"hello":"world","testing":{}},"property":"testing","newValue":{},"currentPath":"testing"}]
+p.testing.blah = 42;		// [{"type":"add","target":{"blah":42},"property":"blah","newValue":42,"currentPath":"testing.blah"}]
+p.arr = [];					// [{"type":"add","target":{"hello":"world","testing":{"blah":42},"arr":[]},"property":"arr","newValue":[],"currentPath":"arr"}]
+p.arr.push("hello world");	// [{"type":"add","target":["hello world"],"property":"0","newValue":"hello world","currentPath":"arr"}]
+console.log(test);			// {"hello":"world","testing":{"blah":42},"arr":["hello world"]}
+delete p.hello;				// [{"type":"delete","target":{"testing":{"blah":42},"arr":["hello world"]},"property":"hello","newValue":null,"previousValue":"world","currentPath":"hello"}]
+p.arr.splice(0,1);			// [{"type":"delete","target":[],"property":"0","newValue":null,"previousValue":"hello world","currentPath":"arr"},{"type":"update","target":[],"property":"length","newValue":0,"previousValue":1,"currentPath":"arr"}]
+console.log(test);			// {"testing":{"blah":42},"arr":[]}
 ```
 
 If you wish to add a second observer function to the same object, you may do so as follows:
@@ -37,3 +38,21 @@ ObservableSlim.observe(p, function(changes) {
 	console.log(changes);
 });
 ```
+
+If you wish to pause the execution of observer functions, you may do so as follows:
+```javascript
+var test = {};
+var p = ObservableSlim.create(test, true, function(changes) {
+	console.log(JSON.stringify(changes));
+});
+ObservableSlim.pause(p);
+```
+
+While an observable is paused, no observer functions will be invoked when the target object is modified.
+
+To resume the execution of observer functions:
+
+```javascript
+ObservableSlim.resume(p);
+```
+
--- observable-slim.js
+++ observable-slim.js
@@ -1,8 +1,7 @@
 /*
  * 	Observable Slim
+ *	Version 0.0.1
  * 	https://github.com/elliotnb/observable-slim
- *
- * 	Version 0.0.1. Copyright 2017, Elliot B. All rights reserved.
  *
  * 	Licensed under the MIT license:
  * 	http://www.opensource.org/licenses/MIT
@@ -10,22 +9,23 @@
  *	Observable Slim is a singleton that allows you to observe changes made to an object and any nested
  *	children of that object. It is intended to assist with one-way data binding, that is, in MVC parlance, 
  *	reflecting changes in the model to the view. Observable Slim aspires to be as lightweight and easily
- *	understood as possible. Minifies down to roughly 500 characters.
+ *	understood as possible. Minifies down to roughly 1500 characters.
  *	Usage:
  *	
  *		var test = {};
- *		var p = ObservableSlim.create(test);
- *
- *		ObservableSlim.observe(p, function(target, property, value, path) {
- *			console.log("Change on '"+path+"', new value: " + JSON.stringify(value));
+ *		var p = ObservableSlim.create(test, true, function(changes) {
+ *			console.log(JSON.stringify(changes));
  *		});
  *		
- *		p.hello = "world";  		// Change on 'hello', new value: "world"
- *		p.testing = {}; 			// Change on 'testing', new value: {}
- *		p.testing.blah = 42;		// Change on 'testing.blah', new value: 42
- *		p.arr = [];					// Change on 'testing.arr', new value: []
- *		p.arr.push("hello world");	// Change on 'testing.arr.0', new value: "hello world"
- *		console.log(test)			// {"hello":"world","testing":{"blah":42},"arr":["hello world"]}
+ *		p.hello = "world";  		// [{"type":"add","target":{"hello":"world"},"property":"hello","newValue":"world","currentPath":"hello"}]
+ *		p.testing = {}; 			// [{"type":"add","target":{"hello":"world","testing":{}},"property":"testing","newValue":{},"currentPath":"testing"}]
+ *		p.testing.blah = 42;		// [{"type":"add","target":{"blah":42},"property":"blah","newValue":42,"currentPath":"testing.blah"}]
+ *		p.arr = [];					// [{"type":"add","target":{"hello":"world","testing":{"blah":42},"arr":[]},"property":"arr","newValue":[],"currentPath":"arr"}]
+ *		p.arr.push("hello world");	// [{"type":"add","target":["hello world"],"property":"0","newValue":"hello world","currentPath":"arr"}]
+ *		console.log(test);			// {"hello":"world","testing":{"blah":42},"arr":["hello world"]}
+ *		delete p.hello;				// [{"type":"delete","target":{"testing":{"blah":42},"arr":["hello world"]},"property":"hello","newValue":null,"previousValue":"world","currentPath":"hello"}]
+ *		p.arr.splice(0,1);			// [{"type":"delete","target":[],"property":"0","newValue":null,"previousValue":"hello world","currentPath":"arr"},{"type":"update","target":[],"property":"length","newValue":0,"previousValue":1,"currentPath":"arr"}]
+ *		console.log(test);			// {"testing":{"blah":42},"arr":[]}
  */
 var ObservableSlim = (function() {
 
@@ -63,6 +63,16 @@ var ObservableSlim = (function() {
 		};
 		
 		var _notifyObservers = function(numChanges) {
+			
+			// determine if observer execution is paused for this observable
+			var i = observables.length;
+			while (i--) {
+				if (observables[i] === observable) {
+					// if the observable is paused, then we don't want to execute any of the observer functions
+					if (observables[i].paused === true) return;
+					break;
+				}
+			};
 		
 			// execute observer functions on a 10ms settimeout, this prevents the observer functions from being executed 
 			// separately on every change -- this is necessary because the observer functions will often trigger UI updates
@@ -144,10 +154,11 @@ var ObservableSlim = (function() {
 		var p = new Proxy(target, handler);
 		
 		// we don't want to create a new observable if this function was invoked recursively
-		if (observable === null) observable = {"target":target, "domDelay":domDelay, "observable":p, "observers":[]};
-		
-		observables.push(observable);
-		
+		if (observable === null) {
+			observable = {"target":target, "domDelay":domDelay, "observable":p, "observers":[],"paused":false};
+			observables.push(observable);
+		}
+
 		return p;
 	};
 	
@@ -183,7 +194,7 @@ var ObservableSlim = (function() {
 		},
 		
 		/*	Method: observe
-				This method
+				This method is used to add a new observer function to an existing observable.
 		
 			Parameters:
 				observable 	- the ES6 Proxy returned by the create() method. We want to observe changes made to this object.
@@ -202,7 +213,40 @@ var ObservableSlim = (function() {
 					break;
 				}
 			};
+		},
+		
+		/*	Method: pause
+				This method will prevent any observer functions from being invoked when a change occurs to observable.
+			
+			Parameters:
+				observable 	- the ES6 Proxy returned by the create() method.
+		*/
+		pause: function(observable) {
+			var i = observables.length;
+			while (i--) {
+				if (observables[i].observable === observable) {
+					observables[i].paused = true;
+					break;
+				}
+			};
+		},
+		
+		/*	Method: resume
+				This method will resume execution of any observer functions when a change is made to observable.
+			
+			Parameters:
+				observable 	- the ES6 Proxy returned by the create() method.
+		*/
+		resume: function(observable) {
+			var i = observables.length;
+			while (i--) {
+				if (observables[i].observable === observable) {
+					observables[i].paused = false;
+					break;
+				}
+			};
 		}
+		
 	};
 
 })();
