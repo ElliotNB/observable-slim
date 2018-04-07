2cb5d1961ca0dd70831e17839a4c92198dc1f7ca
Fabio Ricali <ricalifabio@gmail.com>
2018-04-07 11:29:32 +0200

    Improve change callback, now if it returns false no changes will be made. Works only if domDelay is false.

--- test/test.js
+++ test/test.js
@@ -184,5 +184,43 @@ describe('observable-slim.js', _ => {
 		expect(firstProxy).to.equal(true);
 		expect(secondProxy).to.equal(true);
 	});
-	
+
+    it('Before change, try to add a new string property, but returning false.', () => {
+        ObservableSlim.observe(p, function(changes) {
+            expect(changes[0].type).to.equal("add");
+            expect(changes[0].newValue).to.equal("world");
+            return false;
+        });
+        p.hello = "world";
+        expect(p.hello).to.be.an('undefined');
+        expect(test.hello).to.be.an('undefined');
+    });
+
+    it('Before change, try to modify string property value, but returning false.', () => {
+        ObservableSlim.observe(p, function(changes) {
+            expect(changes[0].type).to.equal("update");
+            expect(changes[0].newValue).to.equal("WORLD");
+            return false;
+        });
+
+        test.hello = "world";
+        p.hello = "WORLD";
+        expect(p.hello).to.equal("world");
+        expect(test.hello).to.equal("world");
+    });
+
+    it('Before change, try to delete a property, but returning false.', () => {
+        ObservableSlim.observe(p, function(changes) {
+            expect(changes[0].type).to.equal("delete");
+            expect(changes[0].property).to.equal("hello");
+
+            return false;
+        });
+
+        test.hello = "hello";
+        delete p.hello;
+
+        expect(p.hello).to.equal("hello");
+        expect(test.hello).to.equal("hello");
+    });
 });
